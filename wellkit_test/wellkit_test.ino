50f64eacd09a20435cb222fb6e1cb7b47cc0b733
keepsky <keepsky@gmail.com>
2024-09-09 16:51:15 +0900

    fix calibration code

--- wellkit_test/wellkit_test.ino
+++ wellkit_test/wellkit_test.ino
@@ -18,37 +18,51 @@
 #include <EEPROM.h> 
 #include "HX711.h"
 
-//#define CALIBRATION_MODE
+#define DEBUG
+
+#define ADDR_CALIBRATION  0
+#define ADDR_CAL_STATUS   10
+
+#define CAL_STATUS_NONE   0
+#define CAL_STATUS_OK     0xAA
+
 #define LBS_TO_GRAM   (453.6)
+
 
 #define DOUT  3
 #define CLK   2
-//HX711 scale(DOUT, CLK);
+
 HX711 scale;
 
-float calibration_factor = 12000;
-
-
+float calibration_factor = 120000;
+int status;
 
 void setup() 
 {
-  scale.begin(DOUT, CLK);
-
-  // read calibration value from EEPROM
-  calibration_factor = get_eeprom();
-
-  pinMode(LED_BUILTIN, OUTPUT);
-  delay(2000);
   Serial.begin(9600);
   delay(2000);
-  //while(!Serial) ;    // 연결을 기다립니다.
-  
-  scale.set_scale(calibration_factor);  //This value is obtained by using calibration step
-  //scale.tare();   //Assuming there is no weight on the scale at start up, reset the scale to 0
 
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  scale.begin(DOUT, CLK);
+
+  status = EEPROM.read(ADDR_CAL_STATUS);
+
+#ifdef DEBUG  
+  Serial.print("status : ");
+  Serial.println(status);
+#endif
+
+  if(status == CAL_STATUS_OK)
+  {
+    scale.set_scale();
+    scale.tare(); //Reset the scale to 0
+
+    // read calibration value from EEPROM
+    calibration_factor = get_eeprom_calibration();  
+    scale.set_scale(calibration_factor);  //This value is obtained by using calibration step
+  }
 }
-
-//#define ORG
 
 char cmd;
 void loop() 
@@ -67,59 +81,38 @@ void loop()
       weight = weight * LBS_TO_GRAM;
       Serial.println(weight, 1);
 
-
-    // moving trailer
-    } else if (cmd == '2'){         
-      blink_builtin_led(50, 3);
-      Serial.println("moving trailer");
-      // TODO : Relay 스위치를 통해서 모터를 구동시키는 코드 필요
-
-
     // get calibration value
-    } else if (cmd == '3'){         
+    } else if (cmd == '2'){         
       Serial.println(calibration_factor);
 
+    } else if (cmd == '3'){         
+      scale.set_scale();
+      scale.tare(); //Reset the scale to 0
+      Serial.println("OK");
 
-#ifdef ORG
     // set calibration value
     } else if (cmd == '4'){         
-      int cal = Serial.parseInt();
-      calibration_factor = cal;
-      set_eeprom(calibration_factor);
+      float value = Serial.parseFloat();
+      calibration_factor = value;
+      set_eeprom_calibration(calibration_factor);
+      scale.set_scale();
+      scale.tare(); //Reset the scale to 0
       scale.set_scale(calibration_factor); //Adjust to this calibration factor
-      //scale.tare();
-      //Serial.println(calibration_factor);
-#else
-    } else if (cmd == '4'){         
-      //scale.set_scale(); //Adjust to this calibration factor
-      scale.tare(20);   
-      Serial.println("OK");
-#endif
-
-#ifdef ORG
-    // 현재 calibration 값으로 영점 조정(tare)
-    // 무게 센서 위에 아무것도 올려 놓지 않은 상태에서 진행해야함
-    } else if (cmd == '5'){         
-      scale.set_scale(calibration_factor); //Adjust to this calibration factor
-      scale.tare();
-#else
-    } else if (cmd == '5'){         
-      int weight = Serial.parseInt();
-      scale.calibrate_scale(weight, 20);
-      calibration_factor = scale.get_scale();
-      scale.set_scale(calibration_factor);
-      set_eeprom(calibration_factor);
       Serial.println(calibration_factor);
 
-#ifdef ORG
-      int cal = Serial.parseInt();
-      long value = LBS_TO_GRAM * scale.get_units(5);
-      calibration_factor = value/cal;
-      scale.set_scale(calibration_factor);
-      set_eeprom(calibration_factor);
-      Serial.println(calibration_factor);
-#endif      
-#endif
+
+    // Open Cover
+    } else if (cmd == '5'){         
+      Serial.println("Open Cover");
+
+    // Close Cover
+    } else if (cmd == '6'){         
+      Serial.println("Close Cover");
+
+    // Get Cover status
+    } else if (cmd == '7'){         
+      Serial.println("get cover status");
+
 
     // get zero factor value
     } else if (cmd == '9'){         
@@ -148,7 +141,7 @@ void blink_builtin_led(int duration, int num)
   }
 }
 
-float get_eeprom(void)
+float get_eeprom_calibration()
 {
   float val;
   char *p = (char*)&val;
@@ -161,7 +154,7 @@ float get_eeprom(void)
   return val;
 }
 
-void set_eeprom(float val)
+void set_eeprom_calibration(float val)
 {
   char *p = (char*)&val;
 
@@ -169,6 +162,8 @@ void set_eeprom(float val)
   {
     EEPROM.write(i, *p++);
   }
+
+  EEPROM.write(ADDR_CAL_STATUS, CAL_STATUS_OK);
 }
 
 
